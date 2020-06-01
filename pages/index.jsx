4157f89036a64b1a58f7a10344bc7dbfb16467d6
Marcus <marcus@Marcuss-MBP.localdomain>
2020-06-01 14:36:19 -0500

    cmt

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Head from 'next/head'
 import { Container, Row, Card, Button } from 'react-bootstrap'
 
+import Menu from '../components/menu/Menu'
+
 
 class Home extends React.Component {
 
@@ -16,6 +18,7 @@ class Home extends React.Component {
                     <title>ReactJS with react-bootstrap</title>
                     <link rel="icon" href="/favicon-32x32.png" />
                 </Head>
+                <Menu></Menu>
 
                 <Container>
                     <p>This is the Home page still.</p>
