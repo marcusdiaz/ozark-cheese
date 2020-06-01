a9fdafc3040c533a01323f36ce839287c158527d
Marcus <marcus@Marcuss-MBP.localdomain>
2020-06-01 15:32:05 -0500

    cmt

--- pages/store.jsx
+++ pages/store.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Row, Card, Button } from 'react-bootstrap'
 import Head from 'next/head'
+import Menu from "../components/menu/Menu";
 
 
 class Store extends React.Component {
@@ -16,6 +17,7 @@ class Store extends React.Component {
                 <title>ReactJS with react-bootstrap</title>
                 <link rel="icon" href="/favicon-32x32.png" />
             </Head>
+            <Menu></Menu>
 
             <Container>
                 <p>This is the Store page still.</p>
